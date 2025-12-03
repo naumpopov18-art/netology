c2e5a9adc0fc414e08f4086d6bbeafb667df50b8
naumpopov18-art <naum.popov@mail.ru>
2025-12-03 18:22:28 +0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,48 @@
+INSERT INTO executors (name) VALUES 
+('Испольнитель 1'),
+('Испольнитель 2'),
+('Испольнитель 3'),
+('Испольнитель 4');
+
+INSERT INTO genres (name) VALUES
+('Жанр 1'),
+('Жанр 2'),
+('Жанр 3');
+
+INSERT INTO albums (name, date) VALUES
+('Альбом 1', '10/10/2020'),
+('Альбом 2', '10/10/2021'),
+('Альбом 3', '10/10/2022');
+
+INSERT INTO tracks (name, duration, albumid) VALUES
+('Трек 1', '100','1'),
+('Трек 2', '100','2'),
+('Трек 3', '100','3'),
+('Трек 4', '100','1'),
+('Трек 5', '100','2'),
+('Трек 6', '100','3');
+
+INSERT INTO collections (name, date) VALUES 
+('Сборник 1', '10/10/2023'),
+('Сборник 2', '10/10/2024'),
+('Сборник 3', '10/10/2019'),
+('Сборник 4', '10/10/2018');
+
+INSERT INTO executorsgenre (genre_id, executor_id) VALUES
+(1, 1),
+(2, 1),
+(2, 2),
+(3, 3),
+(1, 4);
+
+INSERT INTO albumexecutor (album_id, executor_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(1, 4);
+
+INSERT INTO collectiontrack (collections_id, track_id) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
